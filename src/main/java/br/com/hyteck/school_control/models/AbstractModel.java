5e3936b5dda7c4a5263dabaf8f4ea71015446dde
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-05-23 00:15:19 -0300

    feat: Refactor AbstractModel to use Lombok annotations for constructor and field access

--- src/main/java/br/com/hyteck/school_control/models/AbstractModel.java
+++ src/main/java/br/com/hyteck/school_control/models/AbstractModel.java
@@ -1,7 +1,10 @@
 package br.com.hyteck.school_control.models;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
