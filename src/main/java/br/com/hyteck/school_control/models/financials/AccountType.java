11193461ffc1ea5eacff59d09b880928536a7f1d
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-05-31 21:58:59 -0300

    feat: Add AccountType enum to represent financial account types

---
+++ src/main/java/br/com/hyteck/school_control/models/financials/AccountType.java
@@ -0,0 +1,20 @@
+package br.com.hyteck.school_control.models.financials;
+
+/**
+ * Enumeração que representa os tipos de conta financeira.
+ */
+public enum AccountType {
+    /**
+     * Ativo
+     */
+    ASSET,
+    /**
+     * Passivo
+     */
+    LIABILTY,
+    /**
+     * Despesas
+     */
+    EXPENSES,
+    REVENUE
+}
