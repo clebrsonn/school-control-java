e918d1d8d0b2c1c7010b4f298442dd367e29c04c
cleberson.chagas <cleberson.chagas@syngenta.com>
2025-05-31 22:36:31 -0300

    feat: Add AccountType enum to represent financial account types

--- src/main/java/br/com/hyteck/school_control/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/br/com/hyteck/school_control/config/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,14 +18,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JWTProvider tokenProvider;
     private final UserDetailsService userDetailsService;
-
-    public JwtAuthenticationFilter(JWTProvider tokenProvider, UserDetailsService userDetailsService) {
-        this.tokenProvider = tokenProvider;
-        this.userDetailsService = userDetailsService;
-    }
 
 
     /**
